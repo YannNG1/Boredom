525c0353e7d40703e86697399ef100847b1fd8f9
Glitch (site-ennui) <none>
2022-11-24 07:23:12 +0000

    Put javascript code in a js folder ; Repaired font on Ipad

---
+++ js/dark-mode.js
@@ -0,0 +1,24 @@
+function darkMode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+var action = 1;
+
+function darkText() {
+  if (action == 1) {
+    document.getElementById("clock").style.borderColor = "#121212";
+    document.getElementById("clock").style.color = "#121212";
+    document.getElementById("boutton").textContent = "Dark mode";
+    document.getElementById("truc").style.color = "#121212";
+    document.getElementById("truc").style.backgroundImage = "linear-gradient(290deg, #f0f0f0, #ffffff)";
+    action = 2;
+  } else {
+    document.getElementById("clock").style.borderColor = "#f0f0f0";
+    document.getElementById("clock").style.color = "#f0f0f0";
+    document.getElementById("boutton").textContent = "Light mode";
+    document.getElementById("truc").style.color = "#f0f0f0";
+    document.getElementById("truc").style.backgroundImage = "linear-gradient(290deg, #121212, #363636)";
+    action = 1;
+  }
+}
--- js/horloge.js
+++ js/horloge.js
@@ -25,3 +25,5 @@ function showTime() {
 }
 
 showTime();
+
+
